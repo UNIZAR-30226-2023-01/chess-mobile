bd07921a7fbfff0e165b1c86b4ff801b1f4bf7d5
fjpizarro <fjpizarro@localhost.localdomain>
2023-01-29 09:49:05 +0100

    test husky

---
+++ husky.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'hooks': {
+      'pre-commit': 'flutter format ./**/*.dart'
+    }
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+              primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );

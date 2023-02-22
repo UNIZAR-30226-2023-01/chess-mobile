0be9ed2ee1c5e1dfc9e953f256ff4957822529a7
Alvaro <819425@unizar.es>
2023-02-22 12:43:24 +0100

    bug popup fin de partida solucionado

--- lib/components/popups/winner_dialog.dart
+++ lib/components/popups/winner_dialog.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 Object alertaGanador(BuildContext context, bool whiteTurn) {
   return showDialog(
+    barrierDismissible: false,
     context: context,
-    builder: (ctx) => AlertDialog(
+    builder: (BuildContext context) => AlertDialog(
       title: const Text("Partida finalizada"),
       content: Text(
           "Ha ganado el jugador con las piezas ${whiteTurn ? "blancas" : "negras"}"),
       actions: <Widget>[
         ElevatedButton(
           onPressed: () {
-            Navigator.of(ctx).pop();
             Navigator.of(context).pop();
           },
           child: const Text("Ok"),
         ),
       ],
     ),
-  );
+  ).then((value) => Navigator.of(context).pop());
 }

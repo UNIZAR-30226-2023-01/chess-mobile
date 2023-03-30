21936df727dd478c99c55d17447b34ce6896b333
Alvaro <819425@unizar.es>
2023-03-31 00:37:28 +0200

    actualizacion boton play

--- lib/components/buttons/home_play_button.dart
+++ lib/components/buttons/home_play_button.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import '../visual/screen_size.dart';
 
-TextButton playButton(BuildContext context, Function() action) {
-  return TextButton(
-    onPressed: action,
-    child: Container(
-      width: defaultWidth * 0.3,
-      padding: const EdgeInsets.symmetric(vertical: 12.5),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.primary,
-        borderRadius: const BorderRadius.all(Radius.circular(8)),
+SizedBox playButton(BuildContext context, Function() action) {
+  return SizedBox(
+    width: defaultWidth * 0.3,
+    child: Material(
+      color: Theme.of(context).colorScheme.primary,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8)),
       ),
-      child: Center(
-        child: Text(
-          "JUGAR",
-          style: TextStyle(
-            fontSize: 19,
-            fontWeight: FontWeight.bold,
-            color: Theme.of(context).colorScheme.secondary,
+      child: InkWell(
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+        onTap: action,
+        child: Padding(
+          padding: EdgeInsets.all(defaultWidth * 0.04),
+          child: Text(
+            "JUGAR",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 19,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
           ),
         ),
       ),

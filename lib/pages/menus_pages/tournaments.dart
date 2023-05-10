71969e93b0d44db7b02c21c62c76046991618822
Cristian Selivanov <816456@unizar.es>
2023-05-10 22:50:33 +0200

    Estructuras para representar torneos en pantalla con formato de rondas

--- lib/pages/menus_pages/tournaments.dart
+++ lib/pages/menus_pages/tournaments.dart
@@ -1,6 +1,27 @@
 import '../../components/visual/tournament_node.dart';
 import 'package:flutter/material.dart';
 
+// Data structure for matches
+class TournamentMatch {
+  String matchID = "null";
+  String nextMatchID = "null";
+  String startTime = "null";
+  String player1ID = "null";
+  String player2ID = "null";
+  bool played = false;
+}
+
+// Data structure for rounds
+class TournamentData {
+  // Update only first time the tournament itself is fethed
+  List<List> players = List.empty(growable: true);
+  int totalRounds = 1;
+
+  // Update every single time user decides to check another round
+  List<TournamentMatch> matches = List.empty(growable: true);
+  int currentRound = 1;
+}
+
 class TournamentPage extends StatefulWidget {
   const TournamentPage({super.key});
 
